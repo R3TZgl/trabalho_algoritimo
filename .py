12417e96a420f491e2a8103dbb07c355ec7bb3fe
Gabriel Retzlaff <gretzlaff13@gmail.com>
2024-06-19 22:03:00 -0300

    save

---
+++ .py
@@ -0,0 +1,31 @@
+pref1 = "Clemerson"
+pref2 = "Busa"
+pref3 = "Jhon"
+pref4 = "Lucas Lucco"
+
+ver1 = "Colla"
+ver2 = "Honda CIVIC"
+ver3 = "McLovin"
+ver4 = "Panda"
+ver5 = "Primo"
+ver6 = "CDD"
+ver7 = "Chico"
+ver8 = "Pajé"
+ver9 = "Pelagem"
+ver10 = "Samurai"
+
+Npref1 = 10
+Npref2 = 20
+Npref3 = 30
+Npref4 = 40
+
+Nver1 = 10000
+Nver2 = 20000
+Nver3 = 30000
+Nver4 = 40000
+Nver5 = 50000
+Nver6 = 60000
+Nver7 = 70000
+Nver8 = 80000
+Nver9 = 90000
+Nver10 = 11000
