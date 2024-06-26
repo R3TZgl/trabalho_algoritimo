18916d6c2c195b0e910a0392f36f133daa383248
Gabriel Retzlaff <93136568+R3TZgl@users.noreply.github.com>
2024-06-25 22:36:30 -0300

    Atualizar o Urna.py

--- Urna.py
+++ Urna.py
@@ -90,8 +90,7 @@ while urna:
                 print("O eleitor não faz parte desta zona ou seção.")
             else:
                 while verificador_titulo:	
-                    titulo = int(input("Digite o título do eleitor: "))
-                    
+                    titulo = int(input("Digite o título do eleitor: "))                    
                     #como todos os 0 antes de um outro número são desconsiderados, no caso de um título (000000000001) seria igual a (1). Por conta disso foi feito apenas um verificador para não passar os 12 dígitos
 
                     if titulo < 999999999999:
